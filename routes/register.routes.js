684d1aaf4e1d3da8ee02a8b46e8d29d798e979f1
niwatoriDev <esmunozdev@gmail.com>
2022-05-02 19:02:33 -0500

    Register Pet Working Succesfully

--- routes/register.routes.js
+++ routes/register.routes.js
@@ -1,18 +1,23 @@
 const router = require("express").Router();
+const canRegisterPet = require("../middleware/canRegisterPet");
 const Pet = require("../models/Pet.model");
 
 
 /* GET pet registration screen */
-router.get("/pet", (req, res, next) => {
+router.get("/pet", canRegisterPet, (req, res, next) => {
   res.render("register/addPet");
 });
 
 router.post("/pet", (req, res) => {
+console.log(req.session)
+const owner = req.session.user._id
+
+console.log(owner)
+  // return res.send("okey")
  
-  Pet.create(req.body)
+  Pet.create({...req.body, owner})
   .then(newPet => {
-      console.log(newPet)
-      res.redirect("/index")
+      res.redirect("/")
   })
   .catch(err => console.log(err))
 })
