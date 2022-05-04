76bd3aa312ca90a4987901ffba89d23717c808a7
niwatoriDev <esmunozdev@gmail.com>
2022-05-03 19:58:51 -0500

    Added My Vehicles View | Linked Car Creation to User :construct:

--- routes/register.routes.js
+++ routes/register.routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const canRegisterPet = require("../middleware/canRegisterPet");
 const Pet = require("../models/Pet.model");
-const Car = require("../models/Car.model")
+const Car = require("../models/Car.model");
+const User = require("../models/User.model");
 
 
 /* GET pet registration screen */
@@ -31,9 +32,14 @@ const owner = req.session.user._id
  
   Car.create({...req.body, owner})
   .then(newCar => {
-      res.redirect("/cars/carList", { isSession: req.session.user })
+     console.log("el id es:", newCar._id)
+    return User.findByIdAndUpdate(owner, {$push: {car: newCar._id}})
   })
+  .then(() => {
+    res.redirect("/userAssets/cars")
+  })
+
   .catch(err => console.log(err))
-})
+});
 
 module.exports = router;
--- routes/userAssets.routes.js
+++ routes/userAssets.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
+const Car = require("../models/Car.model");
+const User = require("../models/User.model");
 
-/* GET pet list page */
+
+/* GET and Render User's Pet list  */
 router.get("/pets", (req, res, next) => {
   res.render("userAssets/pets/petsList", { isSession: req.session.user });
 });
@@ -8,10 +11,28 @@ router.get("/pets", (req, res, next) => {
 
 
 
-/* GET car list page */
+/* GET and Render User's Car list */
+
 router.get("/cars", (req, res, next) => {
-    res.render("userAssets/cars/carsList", { isSession: req.session.user });
-});
+  
+  const { _id } = req.session.user
+  
+  User.findById(_id)
+  .then((user)  => {
+      console.log(user)
+      const promises = user.car.map((carX) => {
+          return Car.findById(carX)
+      })
+         console.log(promises)  
+         return Promise.all(promises)  
+    })
+    .then(carrosRegistrados => { 
+      res.render("userAssets/cars/carsList", { isSession: req.session.user, carrosRegistrados });
+      // console.log(carrosRegistrados)
+    })
+    .catch(error => console.log("Error:", error))
+  })
+  
 
 
 module.exports = router;
